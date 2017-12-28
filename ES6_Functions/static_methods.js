e5990377016253adec2c6bed3ad805b3fa7eff08
ashwani-luhaniwal <contact.ashwin.cse@gmail.com>
2017-12-27 23:44:23 -0800

    Static Methods in ES6

---
+++ ES6_Functions/static_methods.js
@@ -0,0 +1,13 @@
+/**
+ * Static Methods
+ * ---------------
+ * Static methods are introduced in ES6.
+ * They can be called without creating any instance for the class
+ */
+class Example {
+    static Callme() {
+        console.log("Static method");
+    }
+}
+Example.Callme();
+// Output: Static method
